31e5b8b82ce0b97f9cf35858571de67f1a49c51f
TABASCO <fifabox@gmail.com>
2024-02-25 14:14:12 +0500

    Add level 4

--- Conqueror of tracks/Assets/Sources/Scripts/Game/Fifish.cs
+++ Conqueror of tracks/Assets/Sources/Scripts/Game/Fifish.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class Fifish : MonoBehaviour
 {
     [SerializeField] private AudioClip _celebration;
+    [SerializeField] private ParticleSystem[] _confettis;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.TryGetComponent(out Player.Player player))
         {
-            Fireworks.instance.StartConfetti();
+            foreach (ParticleSystem confettis in _confettis)
+            {
+                confettis.Play();
+            }
+
             AudioSource.PlayClipAtPoint(_celebration,Vector3.zero);
         }
     }
